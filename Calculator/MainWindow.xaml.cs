a15f0a6cf52573b8d8ea02dc14dabea101e009d5
Kuntal Das <25ckuntaldas@gmail.com>
2022-01-24 12:56:26 +0530

    event Handlers

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,9 +20,59 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double lastNumber, result;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            acBtn.Click += AcBtn_Click;
+            ToggleNegativeBtn.Click += ToggleNegativeBtn_Click;
+            percentBtn.Click += PercentBtn_Click;
+            equalBtn.Click += EqualBtn_Click;
+        }
+
+        private void EqualBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void PercentBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!double.TryParse(resultLbl.Content.ToString(), out lastNumber)) return;
+
+            lastNumber /= 100;
+
+            resultLbl.Content = lastNumber;
+        }
+
+        private void ToggleNegativeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!double.TryParse(resultLbl.Content.ToString(), out lastNumber)) return;
+
+            lastNumber *= -1;
+
+            resultLbl.Content = lastNumber;
+        }
+
+        private void AcBtn_Click(object sender, RoutedEventArgs e)
+        {
+            resultLbl.Content = "0";
+        }
+
+        private void sevenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int result;
+            if (!Int32.TryParse(resultLbl.Content.ToString(), out result)) return;
+
+            if (result == 0)
+            {
+                resultLbl.Content = "7";
+            }
+            else
+            {
+                resultLbl.Content = $"{resultLbl.Content}7";
+            }
         }
     }
 }
